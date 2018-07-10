791d240f402d3595d5e76ce54973e0ed76dad0af
jessicaparker13 <parkerjessica2001@gmail.com>
2018-07-10 19:34:00 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -38,10 +38,35 @@ end
 def how_many_steps?
   # Write a loop that fulfills all criteria from Part 3 of the README.md
    steps = 0 
- 
+   loop do
+     puts "#{steps}"
+     if steps % 2 == 0 
+       puts "Left"
+     else
+       puts "Right"
+     end
+     steps += 1 
+     sleep(0.5)
+  end 
+end 
 
 def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
   
-end
+   steps = 0 
+   loop do
+     puts "#{steps}"
+     if steps % 2 == 0 
+       puts "Left"
+     else
+       puts "Right"
+     end
+     steps += 1 
+     sleep(0.5)
+     if steps == 6
+       break 
+  end 
+end 
+  
+
